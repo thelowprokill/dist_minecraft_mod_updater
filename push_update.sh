1b79829d1d719dd91d11f9d038628e9917d11721
jon_is_awesome <jonathan.hull@beefnw.com>
2021-11-05 09:01:14 -0700

    update

--- push_update.sh
+++ push_update.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-cp -r ../../minecraft_mod_updater_src/dist/combined/* ./
+compile_py.sh ../minecraft_mod_updater_src/images/icon.ico ../minecraft_mod_updater_src/config_ui.py
+compile_py.sh ../minecraft_mod_updater_src/images/icon.ico ../minecraft_mod_updater_src/config_ui_admin.py
+compile_py.sh ../minecraft_mod_updater_src/images/icon.ico ../minecraft_mod_updater_src/main_ui.py
+
+cp -r ../minecraft_mod_updater_src/dist/config_ui/* ./
+cp -r ../minecraft_mod_updater_src/dist/config_ui_admin/* ./
+cp -r ../minecraft_mod_updater_src/dist/main_ui/* ./
+#cp -r ../minecraft_mod_updater_src/dist/combined/* ./
+mv main_ui.exe update_mod.exe
 git_ready.sh update
